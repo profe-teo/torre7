06d7562668eaa4a9980781c1e23c78b032713d36
profe-teo <colaboremos@gmail.com>
2025-06-17 20:26:23 +0200

    reloj

---
+++ js/47_ejemplos_varios.js
@@ -0,0 +1,34 @@
+var manejador;
+function iniciar(){
+    muestraHora();
+    manejador = setInterval(muestraHora,1000);
+}
+
+function muestraHora(){
+    let reloj = document.getElementById("reloj");
+
+    let fecha = new Date();
+    const hora = String(fecha.getHours()).padStart(2,'0');
+    let minutos = String(fecha.getMinutes()).padStart(2,'0');
+    let segundos = fecha.getSeconds();
+
+    segundos = segundos.toString().padStart(2,'0');
+
+    reloj.textContent = `${hora}:${minutos}:${segundos}`;
+
+    muestraMes();
+}
+
+function muestraMes(){
+    let dia_mes = document.getElementById("dia_mes");
+    let fecha = new Date();
+    let dia = fecha.getDate();
+    let mes = fecha.getMonth();
+    let anno = fecha.getFullYear();
+
+    dia_mes.textContent = `${dia}-${mes}-${anno}`
+}
+
+function parar(){
+    clearInterval(manejador);
+}
